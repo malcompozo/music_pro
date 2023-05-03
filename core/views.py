5abb3c563ea3de8b57f14bb515f2bfffa469fed2
malcompozo <malcompozo@gmail.com>
2023-05-03 00:17:05 -0400

    agregando RestFramework

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .serializers import UserSerializer, GroupSerializer
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User, Group
 
 # Create your views here.
-
 def home(request):
         return render(request, "core/home.html")
 
@@ -16,3 +18,15 @@ def contact(request):
 
 def sample(request):
         return render(request, "core/sample.html")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
