d2b21a487145ce2e51581a2ab55d9a9f91dfeb8e
malcompozo <malcompozo@gmail.com>
2023-05-15 11:33:33 -0400

    agregando modelos de datos

--- core/api/views.py
+++ core/api/views.py
@@ -1,6 +1,6 @@
 from services.models import Productos
 from .serializers import UserSerializer, GroupSerializer, ProductoSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User, Group
@@ -8,9 +8,12 @@ from django.contrib.auth.models import User, Group
 @api_view(['GET', 'POST'])
 def get_products (request):
     if request.method == 'GET':
-        producto = Productos.objects.all()
-        serializer = ProductoSerializer(producto, many=True)
-        return Response(serializer.data)
+        try:
+            producto = Productos.objects.all()
+            serializer = ProductoSerializer(producto, many=True)
+            return Response(serializer.data)
+        except Productos.DoesNotExist:
+            return Response({'ERROR': 'NO EXISTEN PRODUCTOS INGRESADOS'}, status=status.HTTP_404_NOT_FOUND) # no encontrado
     
     if request.method == 'POST':
         de_serializer = ProductoSerializer(data=request.data)
@@ -18,14 +21,17 @@ def get_products (request):
             de_serializer.save()
             return Response(de_serializer.data)
         else:
-            return Response(de_serializer.errors)
+            return Response(de_serializer.errors, status=status.HTTP_400_BAD_REQUEST) # envio de tipo de dato erroneo
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def get_products_id (request, pk):
     if request.method == 'GET':
-        producto = Productos.objects.get(pk=pk)
-        serializer = ProductoSerializer(producto)
-        return Response(serializer.data)
+        try:
+            producto = Productos.objects.get(pk=pk)
+            serializer = ProductoSerializer(producto)
+            return Response(serializer.data)
+        except Productos.DoesNotExist:
+            return Response({'ERROR': 'NO EXISTEN PRODUCTOS CON ESE ID'}, status=status.HTTP_404_NOT_FOUND) # no encontrado
     
     if request.method == 'PUT': 
         producto = Productos.objects.get(pk=pk)
@@ -34,13 +40,16 @@ def get_products_id (request, pk):
             de_serializer.save()
             return Response(de_serializer.data)
         else:
-            return Response(de_serializer.errors)
+            return Response(de_serializer.errors, status=status.HTTP_400_BAD_REQUEST) # envio de tipo de dato erroneo
            
-    if request.method == 'DELETE':     
-        producto = Productos.objects.get(pk=pk)
-        producto.delete()
-        data={"result":True}
-        return Response(data)
+    if request.method == 'DELETE': 
+        try:    
+            producto = Productos.objects.get(pk=pk)
+            producto.delete()
+        except Productos.DoesNotExist:
+            return Response({'ERROR':' El registro no existe'}, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UserViewSet(viewsets.ModelViewSet):
