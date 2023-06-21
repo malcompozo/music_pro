6692aae4d2e6d73b6635fc408608ac750c93fac0
MatiasLealTapia <matiasa.lealt.23@gmail.com>
2023-06-21 18:32:57 -0400

    cambio metodo de llamada a api cmf

--- services/views.py
+++ services/views.py
@@ -27,22 +27,26 @@ def apis(request):
                 cmf = responseCmf.json()  # Obtener los datos de la respuesta en formato JSON
                 arrayData['cmf'] = cmf
         else:
-                fecha_actual = datetime.now()
-                # Restar un día utilizando timedelta
-                un_dia = timedelta(days=1)
-                fecha_anterior = fecha_actual - un_dia
-                annio = fecha_anterior.year
-                mes = fecha_anterior.month
-                dia = fecha_anterior.day
-                urlCmfRecupero = 'https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar/'+str(annio)+'/'+str(mes)+'/dias/'+str(dia)+'?apikey=02f0f7e74d22234ae406894dddbc6158bb66b8ab&formato=json'
-                responseCmfRecupero = requests.get(urlCmfRecupero)
+                diasMenos = 1
+                successResponse = False
+                while successResponse == False:
+                        fecha_actual = datetime.now()
+                        # Restar un día utilizando timedelta
+                        un_dia = timedelta(days=diasMenos)
+                        fecha_anterior = fecha_actual - un_dia
+                        annio = fecha_anterior.year
+                        mes = fecha_anterior.month
+                        dia = fecha_anterior.day
+                        urlCmfRecupero = 'https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar/'+str(annio)+'/'+str(mes)+'/dias/'+str(dia)+'?apikey=02f0f7e74d22234ae406894dddbc6158bb66b8ab&formato=json'
+                        responseCmfRecupero = requests.get(urlCmfRecupero)
 
-                if responseCmfRecupero.status_code == 200:
-                        cmf = responseCmfRecupero.json()
-                        arrayData['cmf'] = cmf
-                else:
-                        error_msg = f"Error al consumir la API: {responseCmf.status_code}"
-                        return render(request, "services/services.html",{'error':error_msg})
+                        if responseCmfRecupero.status_code == 200:
+                                cmf = responseCmfRecupero.json()
+                                arrayData['cmf'] = cmf
+                                successResponse = True
+                        else:
+                                error_msg = f"Error al consumir la API: {responseCmf.status_code}"
+                                diasMenos += 1
         
         if responseProd.status_code == 200:
                 products = responseProd.json() # Obtener los datos de la respuesta en form
